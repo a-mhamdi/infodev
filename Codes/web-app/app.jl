31a713da40914a2836ab58d91f05b7f5f5be90a3
Moatezhouaidi <168422116+Moatezhouaidi@users.noreply.github.com>
2024-04-29 22:32:02 +1200

    Update app.jl

--- Codes/web-app/app.jl
+++ Codes/web-app/app.jl
@@ -6,13 +6,14 @@ using GenieFramework
     @in N::Int32 = 1000
     @in amp::Float32 = 0.25
     @in freq::Int32 = 1
+    @in ph::float64 = 3.14/100
+    @in offset::Int32 = 0
     
     @out my_sine = PlotData()
 
     @onchange N, amp, freq begin
         x = range(0, 1, length=N)
-        y = amp*sin.(2*π*freq*x)
-        
+        y = amp*sin.(2*π*freq*x.ph).+offset        
         my_sine = PlotData(x=x, 
                            y=y, 
                            plot=StipplePlotly.Charts.PLOT_TYPE_LINE)
